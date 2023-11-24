36b3bfcd82d47412a5101d599eaee8b074998d82
Kent Travis Donovan <kenttravisdonovan@gmail.com>
2023-11-24 21:31:23 +0700

    login register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
